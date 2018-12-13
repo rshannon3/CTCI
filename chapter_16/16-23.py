9d94130d6d339dfc856133668c9a1f8cd250742f
rshannon3 <ryanshannon@protonmail.com>
2018-12-13 15:20:21 -0800

    16-23 revision

--- chapter_16/16-23.py
+++ chapter_16/16-23.py
@@ -8,11 +8,36 @@ import random
 
 def rand5():
 	return random.randint(0,4)
+"""
+First attempt.
+Not even distribution because of adding.
+First and last element will be less likely than the middle
 
 def rand7():
-	r7 = [0,1,2,3,4,5,6]
-	i = (rand5() + rand5()) % 7
+	r7 = [0,1,2,3,4,5,6] * 5
+	i = 0
+	for n in range(0, 7):
+		i += rand5()
 	return r7[i]
+"""
+
+"""
+With no adding, each row and col are equally likely to be picked. We cannot find another 0-6 in the remaining 4 elements,
+so they trigger a re-roll of the row and col.
+"""
+def rand7():
+	r7 = [[0,1,2,3,4],
+		  [5,6,0,1,2],
+		  [3,4,5,6,0],
+		  [1,2,3,4,5],
+		  [6,-1,-1,-1,-1]]
+
+	row = rand5()
+	col = rand5()
+	while row == 4 and col >= 1:
+		row = rand5()
+		col = rand5()
+	return r7[row][col]
 
 #Check for random distribution
 freq = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
